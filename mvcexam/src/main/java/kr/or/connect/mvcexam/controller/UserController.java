1d6b52bfa6776ad4c37ca724c3019e0c67b57706
jisoo1529 <jsjwoo0512@gmail.com>
2019-08-29 16:22:13 +0900

    Controller ex2: RequestMapping,ModelAttribute

---
+++ mvcexam/src/main/java/kr/or/connect/mvcexam/controller/UserController.java
@@ -0,0 +1,22 @@
+package kr.or.connect.mvcexam.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import kr.or.connect.mvcexam.dto.User;
+@Controller
+public class UserController {
+	@RequestMapping(path="/userform", method = RequestMethod.GET) //@GetMapping으로 표현가능함.
+	public String userform() {
+		return "userform";
+	}
+	
+	@RequestMapping(path="/regist", method = RequestMethod.POST)
+	public String regist(@ModelAttribute User user) { //@ModelAttribute: DTO를 통해서 한번에 모든 객체를 받아올 수 있음. 
+		System.out.println("사용자가 입력한 user정보입니다. 해당정보를 이용하는 코드가 와야합니다.");
+		System.out.println(user);
+		return "regist";
+	}
+}
