f525ce76b23cc23a8cb034f7762733d126f4fd00
jisoo1529 <jsjwoo0512@gmail.com>
2019-09-09 17:53:37 +0900

    MVC Interceptor

--- guestbook/src/main/java/kr/or/connect/guestbook/config/WebMvcContextConfiguration.java
+++ guestbook/src/main/java/kr/or/connect/guestbook/config/WebMvcContextConfiguration.java
@@ -5,10 +5,13 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import kr.or.connect.guestbook.interceptor.LogInterceptor;
 
 @Configuration
 @EnableWebMvc
@@ -29,7 +32,7 @@ public class WebMvcContextConfiguration extends WebMvcConfigurerAdapter {
 	}
 	
 	@Override
-	public void addViewControllers(final ViewControllerRegistry registry) {
+	public void addViewControllers(final ViewControllerRegistry registry) {//Controller는 없으나 spring이 내부적으로 사용함. 
 		System.out.println("addViewControllers가 호출됩니다.");
 		registry.addViewController("/").setViewName("index");
 	}
@@ -41,6 +44,9 @@ public class WebMvcContextConfiguration extends WebMvcConfigurerAdapter {
 		resolver.setSuffix(".jsp");
 		return resolver;
 	}
-
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {//인터셉터 등록
+				registry.addInterceptor(new LogInterceptor());
+	}
 }
 
